4002b12235e1d93151db42906107159356c77e3c
Alejo <agiubel@gmail.com>
2023-06-27 22:04:54 -0400

    feat: add email validation

--- lib/extensions/string_extension.dart
+++ lib/extensions/string_extension.dart
@@ -1,7 +1,19 @@
 extension StringExtension on String {
+
+  ///Capitalize first letter
   String get inCapFirst =>
       isNotEmpty ? '${this[0].toUpperCase()}${substring(1)}' : '';
+
+  ///Capitalize all letters
   String get allInCaps => toUpperCase();
+
+  ///Capitalize first letter of each word
   String get capitalizeFirstOfEach =>
       split(' ').map<dynamic>((str) => str.inCapFirst).join(' ');
+
+
+  ///True if is a valid email
+  bool get isEmail => RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+.[a-zA-Z]+",
+      ).hasMatch(this);
 }
